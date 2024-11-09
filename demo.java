4c9a16c4d1589dbb434aa1b266e4bbf20f3f918a
PunithB1601 <punithb072@gmail.com>
2024-11-10 01:53:42 +0530

    class added

--- demo.java
+++ demo.java
@@ -4,4 +4,10 @@ class demo
         String name="Punith";
         System.out.println(name);
     }
+}class demo
+{
+public static void main(String[] args){
+String id=1;
+System.out.println(id);
+}
 }
