6176f5e9c9fbd7f4fd4b70d4dd67d4ea8a3c2c3d
PunithB1601 <punithb072@gmail.com>
2024-11-10 01:55:46 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,11 +1,5 @@
 class demo
 {
-    public static void main(String[] args) {
-        String name="Punith";
-        System.out.println(name);
-    }
-}class demo
-{
 public static void main(String[] args){
 String id=1;
 System.out.println(id);
